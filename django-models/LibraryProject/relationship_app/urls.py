fd74f56231c2a0b845f8052455338874798c5e9f
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-07-20 03:32:11 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,5 @@
 # relationship_app/urls.py
+from .views import list_books, LibraryDetailView
 
 from django.urls import path
 from .views import BookDetailView, LibraryListView  # Replace with your actual views
