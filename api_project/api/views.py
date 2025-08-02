c75e48bc33aaf550a98edf1cf07f7ea470f4313c
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-08-02 01:46:31 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(ListAPIView):
-    queryset = Book.objects.all()  # Fetch all books from the database
-    serializer_class = BookSerializer  # Use the serializer created
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
