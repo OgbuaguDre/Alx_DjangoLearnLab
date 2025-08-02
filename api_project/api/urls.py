cea01e18da410c6b35a864847525d624c79f7f9b
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-08-02 01:54:23 +0100

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
 from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+]
+
+# Create a DRF router instance
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+urlpatterns = [
+    # Old ListAPIView
+    path('books/', BookList.as_view(), name='book-list'),
+
+    # DRF router-generated CRUD routes
+    path('', include(router.urls)),
 ]
