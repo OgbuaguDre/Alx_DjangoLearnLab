bf5e9df79aed9fcbe1aac7ab42a0e5e13f4f752b
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-08-02 02:29:00 +0100

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,6 +3,7 @@ from .views import BookList
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
@@ -17,5 +18,14 @@ urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
 
     # DRF router-generated CRUD routes
+    path('', include(router.urls)),
+]
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+    path('token/', obtain_auth_token, name='api_token_auth'),  # Token endpoint
     path('', include(router.urls)),
 ]
