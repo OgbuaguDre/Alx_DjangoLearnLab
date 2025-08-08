580955b29551cceed42d254275ea1bf2cac71a13
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-08-08 22:47:38 +0100

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,14 +3,32 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 # ListView – retrieve all books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]  # anyone can view list
+    
+    # Enable filtering, search, and ordering
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
 
+    # Step 1: Filtering by specific fields
+    filterset_fields = ['title', 'author', 'publication_year']
+
+    # Step 2: Searching by text match
+    search_fields = ['title', 'author']
+
+    # Step 3: Ordering by certain fields
+    ordering_fields = ['title', 'publication_year']
+
+    # Default ordering
+    ordering = ['title']
 
 # DetailView – retrieve a single book by ID
 class BookDetailView(generics.RetrieveAPIView):
@@ -48,6 +66,7 @@ class BookDeleteView(generics.DestroyAPIView):
     permission_classes = [permissions.IsAuthenticated]  # must be logged in
 
 # Create your views here.
+
 
 
 
