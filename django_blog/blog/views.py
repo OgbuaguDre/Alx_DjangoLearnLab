c29a5fd2d9e4b47ad0d9dadc35f3e1bd917f3c67
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-08-13 20:40:48 +0100

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -9,10 +9,14 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django import forms
+from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 
 
 class CustomUserCreationForm(UserCreationForm):
+ 
     email = forms.EmailField(required=True)
 
     class Meta:
@@ -35,3 +39,57 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'registration/profile.html')
+
+
+
+
+
+from .forms import PostForm
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        # set the author to the currently logged in user
+        return super().form_valid(form.save(commit=False, author=self.request.user))
+
+    # alternative approach if using default form_valid flow:
+    # def form_valid(self, form):
+    #     form.instance.author = self.request.user
+    #     return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        # ensure author isn't changed
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
