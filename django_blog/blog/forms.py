0c4db3c54e1e3bad847636a679f3f6d4f3e88f7a
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-08-14 00:01:29 +0100

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Post
+from .models import Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -17,3 +18,11 @@ class PostForm(forms.ModelForm):
         if commit:
             post.save()
         return post
+
+
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
