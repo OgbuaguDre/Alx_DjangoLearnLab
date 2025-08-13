71075e953fbfb9d90cfdec609e6bf9180dd18b39
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-08-13 20:36:47 +0100

    Create forms.py

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 8}),
+        }
+
+    def save(self, commit=True, author=None):
+        post = super().save(commit=False)
+        if author:
+            post.author = author
+        if commit:
+            post.save()
+        return post
