e88644b3bddb64df175bd0f5964ec0c95ddd4231
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-08-21 01:15:52 +0100

    .

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
 
 class User(AbstractUser):   
     bio = models.TextField(blank=True, null=True)
@@ -12,3 +11,16 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    # Users this user follows (one-way). Reverse name = "followers"
+    following = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='followers',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
